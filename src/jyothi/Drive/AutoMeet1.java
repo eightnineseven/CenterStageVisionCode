ab1b62a98ad5e6cd298646ea66e1fdaa168545fb
eightnineseven <114788956+eightnineseven@users.noreply.github.com>
2023-09-14 11:36:08 -0400

    Update AutoMeet1.java

--- src/jyothi/Drive/AutoMeet1.java
+++ src/jyothi/Drive/AutoMeet1.java
@@ -1,1 +1,7 @@
-
+// Scan the 3 regions for color cube: Color code finished, just need to determine the color we're looking for
+// Drop first pixel on correct tape: Lift movement + encoder movement
+// Move to board: Drive to april tag dependent on original color code
+// drop pixel: lift arm to needed height and release
+// retract lift: lift movement
+// park: encoder movement
+//total points possible: 45
